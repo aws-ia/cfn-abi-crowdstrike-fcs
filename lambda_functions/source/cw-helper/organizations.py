62d7d8702e7bf0c1e1ed52be70fa396bde249193
Ryan Payne <ryanj.payne@outlook.com>
2024-08-19 13:05:33 -0500

    extend orgid function

--- lambda_functions/source/cw-helper/organizations.py
+++ lambda_functions/source/cw-helper/organizations.py
@@ -15,6 +15,7 @@ import cfnresponse
 LOGGER = logging.getLogger(__name__)
 log_level: str = os.environ.get("LOG_LEVEL", "ERROR")
 LOGGER.setLevel(log_level)
+EVENTBUS_ACCOUNT = os.environ.get('EVENTBUS_ACCOUNT')
 
 
 def get_org_id():
@@ -26,8 +27,11 @@ def get_org_id():
     management_account_session = boto3.Session()
     org_client = management_account_session.client("organizations")
     response = org_client.describe_organization()["Organization"]
+    organization_id = response["Id"]
+    parents = org_client.list_parents(ChildId=EVENTBUS_ACCOUNT).get('Parents')
+    eventbus_account_ou = parents[0].get('Id')
     LOGGER.debug({"API_Call": "organizations:DescribeOrganization", "API_Response": response})
-    return {"OrganizationId": response["Id"]}
+    return organization_id, eventbus_account_ou
 
 def lambda_handler(event, context):
     """Lambda Handler.
@@ -37,9 +41,12 @@ def lambda_handler(event, context):
         context: runtime information
     """
     try:
-        data = get_org_id()
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, data, data["OrganizationId"])
+        organization_id, eventbus_account_ou = get_org_id()
+        data_dict = {}
+        data_dict['organization_id'] = organization_id
+        data_dict['eventbus_account_ou'] = eventbus_account_ou
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, data, data_dict)
     except Exception:
         LOGGER.exception("Unexpected!")
         reason = f"See the details in CloudWatch Log Stream: '{context.log_group_name}'"
-        cfnresponse.send(event, context, cfnresponse.FAILED, {}, data["OrganizationId"], reason=reason)
+        cfnresponse.send(event, context, cfnresponse.FAILED, {}, data_dict, reason=reason)
