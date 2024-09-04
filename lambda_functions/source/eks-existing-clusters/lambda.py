cf9449cc1a5bc115a9d943e0fa68942f2d7db16c
Ryan Payne <ryanj.payne@outlook.com>
2024-09-04 10:34:25 -0500

    lint python

--- lambda_functions/source/eks-existing-clusters/lambda.py
+++ lambda_functions/source/eks-existing-clusters/lambda.py
@@ -1,10 +1,11 @@
-import boto3
+"""Function running CrowdStrike EKS Protection for existing EKS Clusters."""
 import json
-import requests
 import os
 import logging
-import botocore
 from datetime import date
+import botocore
+import boto3
+import requests
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -20,6 +21,7 @@ REGION = os.environ['AWS_DEFAULT_REGION']
 SWITCH_ROLE = os.environ['lambda_switch_role']
 
 def accounts():
+    """Function getting AWS Account list."""
     try:
         session = boto3.session.Session()
         client = session.client(
@@ -27,21 +29,23 @@ def accounts():
             region_name=REGION
         )
         response = client.list_accounts()
-        accounts = response['Accounts']
+        response_accounts = response['Accounts']
         next_token = response.get('NextToken', None)
 
         while next_token:
             response = client.list_accounts(NextToken=next_token)
-            accounts += response['Accounts']
+            response_accounts += response['Accounts']
             next_token = response.get('NextToken', None)
 
-        active_accounts = [a for a in accounts if a['Status'] == 'ACTIVE']
+        active_accounts = [a for a in response_accounts if a['Status'] == 'ACTIVE']
         return active_accounts
     except client.exceptions.AccessDeniedException:
-        print("Cannot autodiscover adjacent accounts: cannot list accounts within the AWS organization")
+        print("Cannot autodiscover adjacent accounts: \
+              cannot list accounts within the AWS organization")
         return None
 
 def new_session(account_id, region):
+    """Function establishing boto3 session."""
     try:
         sts_connection = boto3.client('sts')
         credentials = sts_connection.assume_role(
@@ -63,34 +67,37 @@ def new_session(account_id, region):
 
 
 def regions():
+    """Function getting active AWS Regions."""
     session = boto3.session.Session()
     client = session.client(
         service_name='ec2',
         region_name=REGION
     )
 
-    regions = client.describe_regions()['Regions']
-    return regions
+    active_regions = client.describe_regions()['Regions']
+    return active_regions
 
 
 def clusters(session, region_name):
+    """Function getting EKS Clusters."""
     client = session.client(
         service_name='eks',
         region_name=region_name
     )
 
     response = client.list_clusters(maxResults=100)
-    clusters = response['clusters']
+    eks_clusters = response['clusters']
     next_token = response['NextToken'] if 'NextToken' in response else None
 
     while next_token:
         response = client.list_clusters(maxResults=100, NextToken=next_token)
-        clusters += response['clusters']
+        eks_clusters += response['clusters']
         next_token = response['NextToken'] if 'NextToken' in response else None
 
-    return clusters
+    return eks_clusters
 
 def describe_cluster(session, region_name, cluster_name):
+    """Function checking EKS Cluster."""
     client = session.client(
         service_name='eks',
         region_name=region_name
@@ -98,12 +105,15 @@ def describe_cluster(session, region_name, cluster_name):
 
     response = client.describe_cluster(name=cluster_name)
     cluster_arn = response.get('cluster', {}).get('arn')
-    auth_mode = response.get('cluster', {}).get('accessConfig', {}).get('authenticationMode')
-    public_endpoint = response.get('cluster', {}).get('resourcesVpcConfig', {}).get('endpointPublicAccess')
+    auth_mode = response.get('cluster', {}).get('accessConfig', \
+                             {}).get('authenticationMode')
+    public_endpoint = response.get('cluster', {}).get('resourcesVpcConfig', \
+                                   {}).get('endpointPublicAccess')
 
     return cluster_arn, auth_mode, public_endpoint
 
 def check_fargate(session, region_name, cluster_name):
+    """Function checking for Fargate."""
     client = session.client(
         service_name='eks',
         region_name=region_name
@@ -117,15 +127,15 @@ def check_fargate(session, region_name, cluster_name):
         if response['fargateProfileNames'] not in []:
             logger.info('No fargate profiles found, setting node_type to nodegroup...')
             node_type = 'nodegroup'
-            return node_type
         else:
             node_type = 'fargate'
-            return node_type
+        return node_type
     except botocore.exceptions.ClientError as error:
         logger.error(error)
         return None
 
 def start_build(cluster_name, cluster_arn, node_type, account_id, region_name):
+    """Function running CodeBuild for EKS Protection."""
     try:
         session = boto3.session.Session()
         client = session.client(
@@ -169,60 +179,69 @@ def start_build(cluster_name, cluster_arn, node_type, account_id, region_name):
                 }
             ]
         )
-        buildId = build.get('build', {}).get('id')
-        logger.info(f'Started build {PROJECT}, buildId {buildId}')
+        build_id = build.get('build', {}).get('id')
+        logger.info('Started build %s, buildId %s', PROJECT, build_id)
     except botocore.exceptions.ClientError as error:
         logger.error(error)
 
-def cfnresponse_send(event, responseStatus, responseData, physicalResourceId=None, noEcho=False):
-    responseUrl = event['ResponseURL']
-    print(responseUrl)
-    responseBody = {}
-    responseBody['Status'] = responseStatus
-    responseBody['Reason'] = 'See the details in CloudWatch Log Stream: '
-    responseBody['PhysicalResourceId'] = physicalResourceId
-    responseBody['StackId'] = event['StackId']
-    responseBody['RequestId'] = event['RequestId']
-    responseBody['LogicalResourceId'] = event['LogicalResourceId']
-    responseBody['Data'] = responseData
-    json_responseBody = json.dumps(responseBody)
-    print("Response body:\n" + json_responseBody)
+def cfnresponse_send(event, response_status, response_data, physical_resource_id=None):
+    """Function sending response to CloudFormation."""
+    response_url = event['ResponseURL']
+    print(response_url)
+    response_body = {}
+    response_body['Status'] = response_status
+    response_body['Reason'] = 'See the details in CloudWatch Log Stream: '
+    response_body['PhysicalResourceId'] = physical_resource_id
+    response_body['StackId'] = event['StackId']
+    response_body['RequestId'] = event['RequestId']
+    response_body['LogicalResourceId'] = event['LogicalResourceId']
+    response_body['Data'] = response_data
+    json_response_body = json.dumps(response_body)
+    print("Response body:\n" + json_response_body)
     headers = {
         'content-type': '',
-        'content-length': str(len(json_responseBody))
+        'content-length': str(len(json_response_body))
     }
     try:
-        response = requests.put(responseUrl,
-                                data=json_responseBody,
-                                headers=headers)
+        response = requests.put(response_url,
+                                data=json_response_body,
+                                headers=headers,
+                                timeout=5)
         print("Status code: " + response.reason)
     except Exception as e:
         print("send(..) failed executing requests.put(..): " + str(e))
 
 def lambda_handler(event, context):
-    logger.info('Got event {}'.format(event))
-    logger.info('Context {}'.format(context))
+    """Function handler."""
+    logger.info('Got event %s', event)
+    logger.info('Context %s', context)
     logger.info('Gathering Event Details...')
     response_d = {}
     if event["RequestType"] in ["Create"]:
         try:
-            active_regions = regions()
-            active_accounts = accounts()
-            for account in active_accounts:
+            for account in accounts():
                 account_id = account['Id']
-                for region in active_regions:
+                for region in regions():
                     region_name = region["RegionName"]
                     session = new_session(account_id, region_name)
                     if session:
                         for cluster_name in clusters(session, region_name):
 
-                            cluster_arn, auth_mode, public_endpoint = describe_cluster(session, region_name, cluster_name)
+                            cluster_arn, auth_mode, public_endpoint = describe_cluster(session,
+                                                                                       region_name,
+                                                                                       cluster_name)
                             if public_endpoint and 'API' in auth_mode:
                                 node_type = check_fargate(session, region_name, cluster_name)
                                 if node_type:
-                                    start_build(cluster_name, cluster_arn, node_type, account_id, region_name)
+                                    start_build(cluster_name,
+                                                cluster_arn,
+                                                node_type,
+                                                account_id,
+                                                region_name)
                             else:
-                                logger.info(f'Access denied for cluster {cluster_name}. Please verify that API Access and Public Endpoint are enabled')
+                                logger.info('Access denied for cluster %s. \
+                                            Please verify that API Access and Public \
+                                            Endpoint are enabled', cluster_name)
             response_d['status'] = "success"
             cfnresponse_send(event, SUCCESS, response_d, "CustomResourcePhysicalID")
         except botocore.exceptions.ClientError as error:
--- lambda_functions/source/eks-new-clusters/lambda.py
+++ lambda_functions/source/eks-new-clusters/lambda.py
@@ -1,8 +1,9 @@
-import boto3
+"""Function running CrowdStrike EKS Protection for new EKS Clusters."""
 import os
 import logging
-import botocore
 from datetime import date
+import botocore
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -13,6 +14,7 @@ BUCKET = os.environ['artifact_bucket']
 SWITCH_ROLE = os.environ['lambda_switch_role']
 
 def new_session(account_id, region_name):
+    """Function establishing boto3 session."""
     try:
         sts_connection = boto3.client('sts')
         credentials = sts_connection.assume_role(
@@ -33,6 +35,7 @@ def new_session(account_id, region_name):
         return None
 
 def start_build(region, cluster_name, cluster_arn, node_type, account_id, region_name):
+    """Function running CodeBuild for EKS Protection."""
     try:
         session = boto3.session.Session()
         client = session.client(
@@ -76,25 +79,26 @@ def start_build(region, cluster_name, cluster_arn, node_type, account_id, region
                 }
             ]
         )
-        buildId = build.get('build', {}).get('id')
-        logger.info(f'Started build {PROJECT}, buildId {buildId}')
+        build_id = build.get('build', {}).get('id')
+        logger.info('Started build %s, buildId %s', PROJECT, build_id)
     except botocore.exceptions.ClientError as error:
         logger.error(error)
 
 def lambda_handler(event,context):
-    logger.info('Got event {}'.format(event))
-    logger.info('Context {}'.format(context))
-    
+    """Function handler."""
+    logger.info('Got event %s', event)
+    logger.info('Context %s', context)
     logger.info('Gathering Event Details...')
     region_name = event['region']
-    account_id = event['userIdentity']['accountId']
+    account_id = event['detail']['userIdentity']['accountId']
     cluster_name = event['detail']['requestParameters']['name']
     event_name = event['detail']['eventName']
     if 'CreateCluster' in event_name:
         node_type = 'nodegroup'
     elif 'CreateFargateProfile' in event_name:
         node_type = 'fargate'
-
+    else:
+        node_type = 'nodegroup'
     logger.info('Checking EKS Cluster for API Access Config..')
     try:
         session = new_session(account_id, region_name)
@@ -105,11 +109,13 @@ def lambda_handler(event,context):
             name=cluster_name
         )
         cluster_arn = cluster_details.get('cluster', {}).get('arn')
-        auth_mode = cluster_details.get('cluster', {}).get('accessConfig', {}).get('authenticationMode')
-        public_endpoint = cluster_details.get('cluster', {}).get('resourcesVpcConfig', {}).get('endpointPublicAccess')
+        auth_mode = cluster_details.get('cluster', {}).get('accessConfig',\
+                                        {}).get('authenticationMode')
+        public_endpoint = cluster_details.get('cluster', {}).get('resourcesVpcConfig',\
+                                              {}).get('endpointPublicAccess')
         if public_endpoint and 'API' in auth_mode:
             start_build(region_name, cluster_name, cluster_arn, node_type, account_id, region_name)
         else:
-            logger.info(f'API Access not enabled on cluster {cluster_name}')
+            logger.info('API Access not enabled on cluster %s', cluster_name)
     except botocore.exceptions.ClientError as error:
         logger.error(error)
