32cfc6edae40f3e1594d0c888241000b657e53ea
Ryan Payne <ryanj.payne@outlook.com>
2025-01-22 14:12:28 -0600

    bump version and taskcat

--- lambda_functions/source/register-organization/lambda.py
+++ lambda_functions/source/register-organization/lambda.py
@@ -22,7 +22,7 @@ logger.setLevel(logging.INFO)
 SUCCESS = "SUCCESS"
 FAILED = "FAILED"
 
-VERSION = "1.1.1"
+VERSION = "1.2.0"
 NAME = "crowdstrike-cloud-abi"
 USERAGENT = ("%s/%s" % (NAME, VERSION))
 
