5626fb8fdb0a296c1be347ae0d6fdf614a619d59
Ryan Payne <ryanj.payne@outlook.com>
2025-01-31 09:28:30 -0600

    updates

--- lambda_functions/source/ecr-registration/lambda.py
+++ lambda_functions/source/ecr-registration/lambda.py
@@ -30,8 +30,10 @@ PERMISSIONS_BOUNDARY = os.environ['permissions_boundary']
 CROWDSTRIKE_PRINCIPAL = os.environ['crowdstrike_principal']
 GOV_CLOUD = os.environ['gov_cloud']
 VERSION = "1.1.1"
-NAME = "crowdstrike-cloud-abi-ecr"
+NAME = "crowdstrike-cloud-aws-ecr"
 USERAGENT = ("%s/%s" % (NAME, VERSION))
+ROLE_POLICY_ARN = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+ROLE_NAME = "CrowdStrikeECRConnectionRole"
 
 def get_secret():
     """Function to get secret"""
@@ -56,7 +58,7 @@ def create_role(external_id, unique_suffix):
     """Function to create the IAM Role for ECR Registry Connection to CrowdStrike"""
     # commercial_principal = "arn:aws:iam::292230061137:role/CrowdStrikeCustomerRegistryAssessmentRole"
     # govcloud_principal = ""
-    connection_role = f"CrowdStrikeECRConnectionRole-{unique_suffix}"
+    connection_role = f"{ROLE_NAME}-{unique_suffix}"
     trust_policy = json.dumps({
         "Version": "2012-10-17",
         "Statement": [
@@ -80,14 +82,14 @@ def create_role(external_id, unique_suffix):
         role = client.create_role(
             RoleName=connection_role,
             AssumeRolePolicyDocument=trust_policy,
-            Description='ReadOnly role to facilitate ECR Registry connections to CrowdStrike for Image Assessment'
+            Description='ReadOnly role to facilitate ECR Registry connections to CrowdStrike for Image Assessment',
+            PermissionsBoundary=PERMISSIONS_BOUNDARY
         )
     else:
         role = client.create_role(
             RoleName=connection_role,
             AssumeRolePolicyDocument=trust_policy,
-            Description='ReadOnly role to facilitate ECR Registry connections to CrowdStrike for Image Assessment',
-            PermissionsBoundary=PERMISSIONS_BOUNDARY
+            Description='ReadOnly role to facilitate ECR Registry connections to CrowdStrike for Image Assessment'
         )
     client.attach_role_policy(
         RoleName=connection_role,
@@ -110,6 +112,7 @@ def register_ecr(regions, role_arn, external_id, falcon_client_id, falcon_secret
                             )
     for i in regions:
         region_name = i["RegionName"]
+        print(f'Processing region: {region_name}')
         payload = {
             "credential": {
                 "details": {
@@ -120,11 +123,11 @@ def register_ecr(regions, role_arn, external_id, falcon_client_id, falcon_secret
             "type": "ecr",
             "url": f"https://{account}.dkr.ecr.{region_name}.amazonaws.com"
         }
-        print(f'Payload: \n{payload}\n')
+        print(f'Payload: {payload}')
         response = falcon.create_registry_entities(
                                                   body=payload
                                                   )
-        print(f'Payload: \n{response}\n')
+        print(f'Response: {response}')
 
 def get_entities(falcon_client_id, falcon_secret, account):
     """Function to get ECR Registry Connections for this AWS account"""
@@ -133,6 +136,7 @@ def get_entities(falcon_client_id, falcon_secret, account):
                              client_secret=falcon_secret
                             )
     read_response = falcon.read_registry_entities()
+    print(read_response)
     uuids = read_response['body']['resources']
     for i in uuids:
         details = falcon.read_registry_entities_by_uuid(ids=i)
@@ -144,12 +148,14 @@ def get_entities(falcon_client_id, falcon_secret, account):
 
 def delete_entities(falcon_client_id, falcon_secret, local_entities):
     """Function to delete ECR Registry Connections for this AWS account"""
+    print("Deleting registry connections...")
     falcon = FalconContainer(client_id=falcon_client_id,
                              client_secret=falcon_secret
                             )
-    falcon.delete_registry_entities(
+    response = falcon.delete_registry_entities(
                                    ids=local_entities
                                    )
+    print(f'Response: {response}')
 
 def cfnresponse_send(event, response_status, response_data, physical_resource_id=None):
     """Function sending response to CloudFormation."""
@@ -178,6 +184,56 @@ def cfnresponse_send(event, response_status, response_data, physical_resource_id
     except Exception as e:
         print("send(..) failed executing requests.put(..): " + str(e))
 
+def generate_ids():
+    ssm = boto3.client('ssm')
+    external_id = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+    unique_suffix = ''.join(random.choices(string.ascii_letters + string.digits, k=4))
+    try:
+        ssm.put_parameter(
+            Name='crowdstrike-ecr-lambda-role-id',
+            Description='Unique ID for creating CrowdStrike ECR Registry Connection Role',
+            Value=unique_suffix,
+            Type='String',
+            Overwrite=False,
+            Tier='Standard'
+        )
+        ssm.put_parameter(
+            Name='crowdstrike-ecr-lambda-external-id',
+            Description='External ID for creating trust policy for CrowdStrike ECR Registry Connection Role',
+            Value=external_id,
+            Type='String',
+            Overwrite=False,
+            Tier='Standard'
+        )
+    except Exception as e:
+        print("send(..) failed creating SSM Parameters(..): " + str(e))
+    
+    return external_id, unique_suffix
+
+def delete_role():
+    ssm = boto3.client('ssm')
+    iam = boto3.client('iam')
+    print("Deleting IAM Role...")
+    try:
+        response = ssm.get_parameter(
+            Name='crowdstrike-ecr-lambda-role-id'
+        )
+        unique_suffix = response['Parameter']['Value']
+        connection_role = f"{ROLE_NAME}-{unique_suffix}"
+        iam.detach_role_policy(
+            RoleName=connection_role,
+            PolicyArn=ROLE_POLICY_ARN
+        )
+        response = iam.delete_role(
+            RoleName=connection_role
+        )
+        print(f'Response: {response}')
+        ssm.delete_parameter(
+            Name='crowdstrike-ecr-lambda-role-id'
+        )
+    except Exception as e:
+        print("send(..) failed deleting the connection role(..): " + str(e))
+
 def lambda_handler(event, context):
     """Main Function"""
     logger.info('Got event %s' % event)
@@ -191,19 +247,29 @@ def lambda_handler(event, context):
             falcon_client_id = secrets_dict['FalconClientId']
             falcon_secret = secrets_dict['FalconSecret']
             if event['RequestType'] in ['Create']:
-                external_id = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
-                unique_suffix = ''.join(random.choices(string.ascii_letters + string.digits, k=4))
+                external_id, unique_suffix = generate_ids()
                 role_arn = create_role(external_id, unique_suffix)
                 print(f'Created role:\n{role_arn}\n')
                 regions = get_regions()
                 register_ecr(regions, role_arn, external_id, falcon_client_id, falcon_secret, account)
+                print('ECR Connection registration complete!')
                 cfnresponse_send(event, SUCCESS, response, "CustomResourcePhysicalID")
-            elif event['RequestType'] in ['Delete']:
-                local_entities = get_entities(falcon_client_id, falcon_secret, account)
-                delete_entities(falcon_client_id, falcon_secret, local_entities)
+            # elif event['RequestType'] in ['Delete']:
+            #     local_entities = get_entities(falcon_client_id, falcon_secret, account)
+            #     delete_entities(falcon_client_id, falcon_secret, local_entities)
+            #     delete_role()
+            #     print("Complete!")
+            #     cfnresponse_send(event, SUCCESS, response, "CustomResourcePhysicalID")
+            else:
+                print("complete")
                 cfnresponse_send(event, SUCCESS, response, "CustomResourcePhysicalID")
         else:
+            print("complete")
             cfnresponse_send(event, SUCCESS, response, "CustomResourcePhysicalID")
     except Exception as err:
         logger.info('Registration Failed %s' % err)
-        cfnresponse_send(event, FAILED, err, "CustomResourcePhysicalID")
+        error = str(err)
+        response = {
+            "error": error
+        }
+        cfnresponse_send(event, FAILED, response, "CustomResourcePhysicalID")
