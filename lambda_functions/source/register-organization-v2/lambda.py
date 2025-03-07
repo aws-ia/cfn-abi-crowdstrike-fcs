a10f6c9efb2e17b1bb39b89c88dfc8368391e405
Ryan Payne <ryan.payne@crowdstrike.com>
2025-03-07 10:03:14 -0600

    bump version

--- lambda_functions/source/register-organization-v2/lambda.py
+++ lambda_functions/source/register-organization-v2/lambda.py
@@ -22,7 +22,7 @@ logger.setLevel(logging.INFO)
 SUCCESS = "SUCCESS"
 FAILED = "FAILED"
 
-VERSION = "1.2.0"
+VERSION = "1.2.1"
 NAME = "crowdstrike-cloud-abi"
 USERAGENT = ("%s/%s" % (NAME, VERSION))
 
