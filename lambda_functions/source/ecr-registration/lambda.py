94cb4372e5b8ecb1b813fb39ad2ee7fba079154b
Ryan Payne <ryanj.payne@outlook.com>
2025-06-24 10:11:08 -0500

    Feat/gov ecr connections (#84)
    * update ecr function
    
    * return regions string
    
    * update reader role
    
    * update ecr stackset params
    
    * update params and env variables
    
    * bump version
    
    * doc updates
    
    * lint
    
    * remove requests lib

--- lambda_functions/source/ecr-registration/lambda.py
+++ lambda_functions/source/ecr-registration/lambda.py
@@ -8,8 +8,9 @@ import base64
 import subprocess
 import random
 import string
+import time
 import boto3
-import requests
+import urllib3
 from botocore.exceptions import ClientError
 
 logger = logging.getLogger()
@@ -28,15 +29,20 @@ SECRET_STORE_NAME = os.environ['secret_name']
 SECRET_STORE_REGION = os.environ['secret_region']
 PERMISSIONS_BOUNDARY = os.environ['permissions_boundary']
 CROWDSTRIKE_PRINCIPAL = os.environ['crowdstrike_principal']
-GOV_CLOUD = os.environ['gov_cloud']
+GOV_CLOUD_PRINCIPAL = os.environ['gov_cloud_principal']
+GOV_CLOUD = eval(os.environ['gov_cloud'])
+COMM_TO_GOV_CLOUD = eval(os.environ['comm_to_gov_cloud'])
 STACK_ID = os.environ['stack_id']
-VERSION = "1.2.1"
+VERSION = "1.5.0"
 NAME = "crowdstrike-cloud-aws-ecr"
 USERAGENT = ("%s/%s" % (NAME, VERSION))
-ROLE_POLICY_ARN = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+ROLE_POLICY_ARN = os.environ['role_policy_arn']
 ROLE_NAME = "CrowdStrikeECRConnectionRole"
+DISCONNECT_UPON_DELETE = eval(os.environ['disconnect_upon_delete'])
+CS_CLOUD = os.environ['cs_cloud']
+REGIONS = os.environ['regions']
 
-def get_secret():
+def get_secret(event):
     """Function to get secret"""
     session = boto3.session.Session()
     client = session.client(
@@ -48,25 +54,33 @@ def get_secret():
             SecretId=SECRET_STORE_NAME
         )
     except ClientError as e:
-        raise e
+        error = str(e)
+        response = {
+            "error": error
+        }
+        cfnresponse_send(event, context, FAILED, response, "CustomResourcePhysicalID")
     if 'SecretString' in get_secret_value_response:
         secret = get_secret_value_response['SecretString']
     else:
         secret = base64.b64decode(get_secret_value_response['SecretBinary'])
     return secret
 
-def create_role(external_id, STACK_ID):
+def create_role(external_id):
     """Function to create the IAM Role for ECR Registry Connection to CrowdStrike"""
-    # commercial_principal = "arn:aws:iam::292230061137:role/CrowdStrikeCustomerRegistryAssessmentRole"
-    # govcloud_principal = ""
     connection_role = f"{ROLE_NAME}-{STACK_ID}"
+    if GOV_CLOUD:
+        principal=GOV_CLOUD_PRINCIPAL
+    elif COMM_TO_GOV_CLOUD:
+        principal=GOV_CLOUD_PRINCIPAL
+    else:
+        principal=CROWDSTRIKE_PRINCIPAL
     trust_policy = json.dumps({
         "Version": "2012-10-17",
         "Statement": [
             {
                 "Effect": "Allow",
                 "Principal": {
-                    "AWS": CROWDSTRIKE_PRINCIPAL
+                    "AWS": principal
                 },
                 "Action": "sts:AssumeRole",
                 "Condition": {
@@ -77,7 +91,6 @@ def create_role(external_id, STACK_ID):
             }
         ]
     })
-
     client = boto3.client('iam')
     if PERMISSIONS_BOUNDARY:
         role = client.create_role(
@@ -94,36 +107,70 @@ def create_role(external_id, STACK_ID):
         )
     client.attach_role_policy(
         RoleName=connection_role,
-        PolicyArn='arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly'
+        PolicyArn=ROLE_POLICY_ARN
     )
-
     return role['Role']['Arn']
 
 def get_regions():
     """Function getting active AWS Regions."""
-    client = boto3.client('ec2')
-    regions = client.describe_regions()['Regions']
-
+    regions = []
+    if REGIONS:
+        regions = REGIONS.split(',')
+    else:
+        client = boto3.client('ec2')
+        response = client.describe_regions()['Regions']
+        for i in response['Regions']:
+            regions.append(i['RegionName'])
     return regions
 
 def register_ecr(regions, role_arn, external_id, falcon_client_id, falcon_secret, account):
     """Function to register ECR in each region with CrowdStrike"""
     falcon = FalconContainer(client_id=falcon_client_id,
-                             client_secret=falcon_secret
+                             client_secret=falcon_secret,
+                             base_url=CS_CLOUD
                             )
     for i in regions:
-        region_name = i["RegionName"]
+        region_name = i.strip()
         print(f'Processing region: {region_name}')
-        payload = {
-            "credential": {
-                "details": {
-                    "aws_iam_role": role_arn,
-                    "aws_external_id": external_id
-                }
-            },
-            "type": "ecr",
-            "url": f"https://{account}.dkr.ecr.{region_name}.amazonaws.com"
-        }
+        if GOV_CLOUD:
+            payload = {
+                "all_repositories_monitored": True,
+                "url": f"https://{account}.dkr.ecr.{region_name}.amazonaws.com",
+                "credential": {
+                    "details": {
+                        "aws_iam_role": role_arn,
+                        "aws_external_id": external_id,
+                        "aws_gov_using_commercial_connection": False
+                    },
+                    "type": "ecr"
+                },
+                "type": "ecr"
+            }
+        elif COMM_TO_GOV_CLOUD:
+            payload = {
+                "all_repositories_monitored": True,
+                "url": f"https://{account}.dkr.ecr.{region_name}.amazonaws.com",
+                "credential": {
+                    "details": {
+                        "aws_iam_role": role_arn,
+                        "aws_external_id": external_id,
+                        "aws_gov_using_commercial_connection": True
+                    },
+                    "type": "ecr"
+                },
+                "type": "ecr"
+            }
+        else:
+            payload = {
+                "credential": {
+                    "details": {
+                        "aws_iam_role": role_arn,
+                        "aws_external_id": external_id
+                    }
+                },
+                "type": "ecr",
+                "url": f"https://{account}.dkr.ecr.{region_name}.amazonaws.com"
+            }
         print(f'Payload: {payload}')
         response = falcon.create_registry_entities(
                                                   body=payload
@@ -134,7 +181,8 @@ def get_entities(falcon_client_id, falcon_secret, account):
     """Function to get ECR Registry Connections for this AWS account"""
     local_entities = []
     falcon = FalconContainer(client_id=falcon_client_id,
-                             client_secret=falcon_secret
+                             client_secret=falcon_secret,
+                             base_url=CS_CLOUD
                             )
     read_response = falcon.read_registry_entities()
     print(read_response)
@@ -144,86 +192,78 @@ def get_entities(falcon_client_id, falcon_secret, account):
         url = details['body']['resources'][0]['url']
         if account in url:
             local_entities += [i]
-
     return local_entities
 
 def delete_entities(falcon_client_id, falcon_secret, local_entities):
     """Function to delete ECR Registry Connections for this AWS account"""
     print("Deleting registry connections...")
     falcon = FalconContainer(client_id=falcon_client_id,
-                             client_secret=falcon_secret
+                             client_secret=falcon_secret,
+                             base_url=CS_CLOUD
                             )
     response = falcon.delete_registry_entities(
                                    ids=local_entities
                                    )
     print(f'Response: {response}')
 
-def cfnresponse_send(event, response_status, response_data, physical_resource_id=None):
+http = urllib3.PoolManager()
+
+def cfnresponse_send(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False, reason=None):
     """Function sending response to CloudFormation."""
-    response_url = event['ResponseURL']
-    print(response_url)
-    response_body = {}
-    response_body['Status'] = response_status
-    response_body['Reason'] = 'See the details in CloudWatch Log Stream: '
-    response_body['PhysicalResourceId'] = physical_resource_id
-    response_body['StackId'] = event['StackId']
-    response_body['RequestId'] = event['RequestId']
-    response_body['LogicalResourceId'] = event['LogicalResourceId']
-    response_body['Data'] = response_data
-    json_response_body = json.dumps(response_body)
-    print("Response body:\n" + json_response_body)
+    responseUrl = event['ResponseURL']
+    print(responseUrl)
+    responseBody = {
+        'Status' : responseStatus,
+        'Reason' : reason or "See the details in CloudWatch Log Group: {}".format(context.log_group_name),
+        'PhysicalResourceId' : physicalResourceId or context.log_stream_name,
+        'StackId' : event['StackId'],
+        'RequestId' : event['RequestId'],
+        'LogicalResourceId' : event['LogicalResourceId'],
+        'NoEcho' : noEcho,
+        'Data' : responseData
+    }
+    json_responseBody = json.dumps(responseBody)
+    print("Response body:")
+    print(json_responseBody)
     headers = {
-        'content-type': '',
-        'content-length': str(len(json_response_body))
+        'content-type' : '',
+        'content-length' : str(len(json_responseBody))
     }
     try:
-        response = requests.put(response_url,
-                                data=json_response_body,
-                                headers=headers,
-                                timeout=5)
-        print("Status code: " + response.reason)
+        response = http.request('PUT', responseUrl, headers=headers, body=json_responseBody)
+        print("Status code:", response.status)
     except Exception as e:
-        print("send(..) failed executing requests.put(..): " + str(e))
+        print("send(..) failed executing http.request(..):", e)
 
 def generate_ids():
     ssm = boto3.client('ssm')
     external_id = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
-    try:
-        ssm.put_parameter(
-            Name=f'crowdstrike-ecr-lambda-external-id{STACK_ID}',
-            Description='External ID for creating trust policy for CrowdStrike ECR Registry Connection Role',
-            Value=external_id,
-            Type='String',
-            Overwrite=False,
-            Tier='Standard'
-        )
-    except Exception as e:
-        print("send(..) failed creating SSM Parameters(..): " + str(e))
-    
+    ssm.put_parameter(
+        Name=f'crowdstrike-ecr-lambda-external-id-{STACK_ID}',
+        Description='External ID for creating trust policy for CrowdStrike ECR Registry Connection Role',
+        Value=external_id,
+        Type='String',
+        Overwrite=False,
+        Tier='Standard'
+    )
     return external_id
 
 def delete_role():
     ssm = boto3.client('ssm')
     iam = boto3.client('iam')
     print("Deleting IAM Role...")
-    try:
-        response = ssm.get_parameter(
-            Name='crowdstrike-ecr-lambda-role-id'
-        )
-        connection_role = f"{ROLE_NAME}-{STACK_ID}"
-        iam.detach_role_policy(
-            RoleName=connection_role,
-            PolicyArn=ROLE_POLICY_ARN
-        )
-        response = iam.delete_role(
-            RoleName=connection_role
-        )
-        print(f'Response: {response}')
-        ssm.delete_parameter(
-            Name=f'crowdstrike-ecr-lambda-external-id{STACK_ID}'
-        )
-    except Exception as e:
-        print("send(..) failed deleting the connection role(..): " + str(e))
+    connection_role = f"{ROLE_NAME}-{STACK_ID}"
+    iam.detach_role_policy(
+        RoleName=connection_role,
+        PolicyArn=ROLE_POLICY_ARN
+    )
+    response = iam.delete_role(
+        RoleName=connection_role
+    )
+    print(f'Response: {response}')
+    ssm.delete_parameter(
+        Name=f'crowdstrike-ecr-lambda-external-id-{STACK_ID}'
+    )
 
 def lambda_handler(event, context):
     """Main Function"""
@@ -232,35 +272,37 @@ def lambda_handler(event, context):
     account = boto3.client('sts').get_caller_identity().get('Account')
     response = {}
     try:
-        secret_str = get_secret()
+        secret_str = get_secret(event)
         if secret_str:
             secrets_dict = json.loads(secret_str)
             falcon_client_id = secrets_dict['FalconClientId']
             falcon_secret = secrets_dict['FalconSecret']
             if event['RequestType'] in ['Create']:
                 external_id = generate_ids()
-                role_arn = create_role(external_id, STACK_ID)
+                role_arn = create_role(external_id)
                 print(f'Created role:\n{role_arn}\n')
                 regions = get_regions()
+                time.sleep(60)
                 register_ecr(regions, role_arn, external_id, falcon_client_id, falcon_secret, account)
                 print('ECR Connection registration complete!')
-                cfnresponse_send(event, SUCCESS, response, "CustomResourcePhysicalID")
-            # elif event['RequestType'] in ['Delete']:
-            #     local_entities = get_entities(falcon_client_id, falcon_secret, account)
-            #     delete_entities(falcon_client_id, falcon_secret, local_entities)
-            #     delete_role()
-            #     print("Complete!")
-            #     cfnresponse_send(event, SUCCESS, response, "CustomResourcePhysicalID")
+                cfnresponse_send(event, context, SUCCESS, response, "CustomResourcePhysicalID")
+            elif event['RequestType'] in ['Delete']:
+                if DISCONNECT_UPON_DELETE:
+                    local_entities = get_entities(falcon_client_id, falcon_secret, account)
+                    delete_entities(falcon_client_id, falcon_secret, local_entities)
+                    delete_role(event)
+                    print("Complete!")
+                cfnresponse_send(event, context, SUCCESS, response, "CustomResourcePhysicalID")
             else:
                 print("complete")
-                cfnresponse_send(event, SUCCESS, response, "CustomResourcePhysicalID")
+                cfnresponse_send(event, context, SUCCESS, response, "CustomResourcePhysicalID")
         else:
             print("complete")
-            cfnresponse_send(event, SUCCESS, response, "CustomResourcePhysicalID")
+            cfnresponse_send(event, context, SUCCESS, response, "CustomResourcePhysicalID")
     except Exception as err:
         logger.info('Registration Failed %s' % err)
         error = str(err)
         response = {
             "error": error
         }
-        cfnresponse_send(event, FAILED, response, "CustomResourcePhysicalID")
+        cfnresponse_send(event, context, FAILED, response, "CustomResourcePhysicalID")
